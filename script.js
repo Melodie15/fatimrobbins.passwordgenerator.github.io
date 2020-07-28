fc1aef95365d7efb278213140f2e594ecc1211e3
frobbins <robbinsfatim@gmail.com>
2020-07-27 19:47:59 -0600

    Created variables for letters, numbers and characters

---
+++ script.js
@@ -0,0 +1,12 @@
+// letter and character variables
+var capLetter = ["A", "B", "C", "D", "E", "F", "G", "I", "H","J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+var character = ["!", "@", "#", "$", "%", "^", "&", "*", "?"];
+
+var letter = ["a", "b", "c", "d", "e", "f", "g", "i", "h","J", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+for (var i = 0; i < character.length; i++) {
+    console.log(character[i]);
+  }
