ff34df3f46e0cff691981d20d7630ec138443565
frobbins <robbinsfatim@gmail.com>
2020-07-28 18:19:53 -0600

    Added new function for all variables

--- script.js
+++ script.js
@@ -9,14 +9,18 @@ var number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
 var randomString = '';
 
-// var allOptions = [capLetter, character, letter, number];
+
+var allOptions = [capLetter, character, letter, number];
 
 
 function Generate(){
-    prompt("How long would you like your password to be?");
-    prompt("Would you like special characters?");
-    prompt("Would you like uppercase letters?");
-    prompt("Would you like numbers");
+    var charactChoice = prompt("How many characters would you like? 8 or 10?", "enter");
+    var specCharChoice = prompt("Would you like special characters?","enter y or n");
+    var upperChoice = prompt("Would you like uppercase letters?", "enter y or n");
+    var numChoice = prompt("Would you like numbers", "enter y or n");
+
+    if (charactChoice === "8" || charactChoice === "10" && specCharChoice === "y" && upperChoice === "y" && numChoice === "y") {
+
 
     for(var i=0;i<2;i++){
         var rCaplet = Math.floor(Math.random()*capLetter.length);
@@ -34,6 +38,16 @@ function Generate(){
         var chart = Math.floor(Math.random()*character.length);
         randomString += character[chart];
     }
- alert(randomString);
+
+    // radomString = document.getElementById("results").innerHTML;
     
+    console.log(randomString);
 }
+}
+
+    // var randomString = document.querySelectorAll("results");
+    // randomString.textContent;
+    // document.getElementById("results").textContent(randomString);
+    // randomString();
+    
+
